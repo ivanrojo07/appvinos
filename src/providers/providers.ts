984596032aa26459443b8ec7fa030f0f404f639b
Ivan Rojo <ivanrojo07@gmail.com>
2018-06-01 13:52:04 -0500

    providers uva y vinicolas

--- src/providers/providers.ts
+++ src/providers/providers.ts
@@ -2,10 +2,14 @@ import {UsuarioServiceProvider} from './usuario-service/usuario-service';
 import { TarjetaProvider } from './tarjeta/tarjeta';
 import { DomicilioFiscalProvider } from './domicilio-fiscal/domicilio-fiscal';
 import { DomicilioEnvioProvider } from './domicilio-envio/domicilio-envio';
+import { ProductoresProvider } from './productores/productores';
+import { UvasProvider } from './uvas/uvas';
 
 export{
     UsuarioServiceProvider,
     TarjetaProvider,
     DomicilioFiscalProvider,
     DomicilioEnvioProvider,
+    ProductoresProvider,
+    UvasProvider,
 };
--- src/providers/uvas/uvas.ts
+++ src/providers/uvas/uvas.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
 
 /*
   Generated class for the UvasProvider provider.
@@ -11,8 +12,19 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class UvasProvider {
 
+  public url: string;
+
   constructor(public http: Http) {
     console.log('Hello UvasProvider Provider');
+    this.url = "http://byw.from-tn.com/pwn/api/uvas";
+  }
+
+  getUvas(){
+    let headers = new Headers({
+      'Content-Type' : "application/json",
+      'Accept' : "application/json"
+    });
+    return this.http.get(this.url,{headers:headers}).map(res=>res.json());
   }
 
 }
