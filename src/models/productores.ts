22790b3e59ea058bf3c8c43cd78f1de9f739a11a
Ivan Rojo <ivanrojo07@gmail.com>
2018-06-03 15:23:19 -0500

    Marker

---
+++ src/models/productores.ts
@@ -0,0 +1,23 @@
+export class Productores{
+    constructor(
+        public id: number,
+        public nombre: string,
+        public distinciones: string,
+        public inicio: number,
+        public filosofia: string,
+        public locacion: string,
+        public long: number,
+        public lat: number,
+        public enologo: string,
+        public wine_maker: string,
+        public contacto: string,
+        public puesto: string,
+        public correo: string,
+        public celular: string,
+        public telefono: string,
+        public observaciones: string,
+        public fecha_observacion: string,
+    ){
+
+    }
+}
--- src/pages/productores/productores.ts
+++ src/pages/productores/productores.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angu
 import {
   GoogleMaps, GoogleMap, GoogleMapsEvent, GoogleMapOptions, 
   Marker } from '@ionic-native/google-maps';
+import { Productores } from "../../models/productores";
 import { ProductoresProvider } from '../../providers/providers';
 /**
  * Generated class for the ProductoresPage page.
@@ -10,25 +11,7 @@ import { ProductoresProvider } from '../../providers/providers';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-export interface productores{
-  id: number,
-  nombre: string,
-  distinciones: string,
-  inicio: number,
-  filosofia: string,
-  locacion: string,
-  long: number,
-  lat: number,
-  enologo: string,
-  wine_maker: string,
-  contacto: string,
-  puesto: string,
-  correo: string,
-  celular: string,
-  telefono: string,
-  observaciones: string,
-  fecha_observacion: string,
-}
+
 
 @IonicPage()
 @Component({
@@ -39,36 +22,38 @@ export interface productores{
 export class ProductoresPage implements OnInit {
 
   map: GoogleMap;
-  productores: productores[];
+  marker: Marker
+  productores: Array<Productores>;
   messageError: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private productoresProvider: ProductoresProvider, public alertCtrl: AlertController,) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public productoresProvider: ProductoresProvider, public alertCtrl: AlertController,) {
     this.productores = [];
-    this.productoresProvider.getProductores().subscribe(result => {
-      this.productores = result.vinicolas;
-      this.productores.forEach(productor => {
-        this.alert('Vinicola:', productor.lat.toString());
-      })
-    }, error => {
-      this.messageError = JSON.parse(error._body)
-      console.log("Error " + JSON.stringify(this.messageError));
-    });
-  }
-  ngOnInit(){
-    this.showProductores();
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ProductoresPage');
-    // this.showProductores();
-    this.loadMap();
-  }
-
-  showProductores(){
     
   }
+  ngOnInit(){
+    // this.showProductores();
+  }
+  
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ProductoresPage');
+    this.loadMap();
+    // this.showProductores();
+  }
+
+  // showProductores(){
+  //   this.productoresProvider.getProductores().subscribe(result => {
+  //     this.productores = result.vinicolas;
+  //     this.alert('Vinicola:', this.productores.length.toString());
+  //     this.productores.forEach(productor => {
+  //     })
+  //   }, error => {
+  //     this.messageError = JSON.parse(error._body)
+  //     console.log("Error " + JSON.stringify(this.messageError));
+  //   });
+  // }
   
   loadMap(){
+    // this.showProductores();
     let mapOptions : GoogleMapOptions = {
       camera: {
         target: {
@@ -82,38 +67,28 @@ export class ProductoresPage implements OnInit {
     this.map = GoogleMaps.create('map_canvas', mapOptions);
     
     // this.showProductores();
-    for (let vinicola of this.productores) {
-      let marker: Marker = this.map.addMarkerSync({
-        title: '@ionic-native/google-maps',
-        icon: 'blue',
-        animation: 'DROP',
-        position: {
-          lat: 19.498152811951056+vinicola.id,
-          lng: -99.1275487497262+vinicola.id
-        }
-      });
-      
-    }
+    
 
     // marker.showInfoWindow();
-
-    this.productores.forEach(productor=>{
-      let marker: Marker = this.map.addMarkerSync({
-        title: productor.nombre,
-        icon: 'red',
-        animation: 'DROP',
-        position: {
-          lat: productor.lat,
-          lng: productor.long
-        }
+    
+    this.productoresProvider.getProductores().subscribe(result => {
+      this.productores = result.vinicolas;
+      // this.alert('Vinicola:', this.productores.length.toString());
+      this.productores.forEach(productor => {
+        this.map.addMarker({
+          title: productor.nombre,
+          icon: 'red',
+          animation: 'DROP',
+          position: {
+            lat: productor.lat,
+            lng: productor.long
+          }
+        }).then((marker: Marker) => { this.alert("Marker", marker.getId()) });
       });
-
-      marker.showInfoWindow();
-
+    }, error => {
+      this.messageError = JSON.parse(error._body)
+      console.log("Error " + JSON.stringify(this.messageError));
     });
-    
-      
-    
 
   }
   public alert(titulo: string, contenido: string) {
